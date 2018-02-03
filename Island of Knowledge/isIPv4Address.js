3182ee10e7d7f31087a4e0349ac0c3ddf237f9c9
Arman Sujoyan <asujoyan@gmail.com>
2018-02-03 12:03:05 +0400

    Added solution for isIPv4Address check.

---
+++ Island of Knowledge/isIPv4Address.js
@@ -0,0 +1,6 @@
+// For the sake of simplicity, we will just use RegExp in
+// order to check the validity of the givent IPv4 address.
+function isIPv4Address(inputString) {
+    return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(inputString)
+    // Return the checked result
+}
